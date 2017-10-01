58274486a715902f16e347938b5e00f0ef5d23e9
Thiago Nogueira <thiago.droz@gmail.com>
2017-10-01 17:28:56 -0300

    Creating 'first', 'tail', 'last' and 'init' methods

--- test/library.spec.js
+++ test/library.spec.js
@@ -12,6 +12,72 @@ describe('Given an instance of my library', () => {
     });
   });
 
+  describe('when the method "first" is called', () => {
+    it('should return undefined if something different of a list is passed', () => {
+      expect(OhMyLib.first(1)).to.be.undefined;
+    });
+    it('should return undefined if an empty list is passed', () => {
+      expect(OhMyLib.first([])).to.be.undefined;
+    });
+    it('should return the unique element if a list with a single element is passed', () => {
+      expect(OhMyLib.first([5])).to.be.equal(5);
+    });
+    it('should return the first element if a list with more than one element is passed', () => {
+      expect(OhMyLib.first([5,10,2,9])).to.be.equal(5);
+    });
+  });
+
+  describe('when the method "tail" is called', () => {
+    it('should return undefined if something different of a list is passed', () => {
+      expect(OhMyLib.tail(1)).to.be.undefined;
+    });
+    it('should return undefined if an empty list is passed', () => {
+      expect(OhMyLib.tail([])).to.be.undefined;
+    });
+    it('should return an empty list if a list with a single element is passed', () => {
+      expect(OhMyLib.tail([5])).to.be.empty;
+    });
+    it('should return the tail if a list with more than one element is passed', () => {
+      const list = [5,10,2,9];
+      expect(OhMyLib.tail(list)).to.contains(10);
+      expect(OhMyLib.tail(list)).to.contains(2);
+      expect(OhMyLib.tail(list)).to.contains(9);
+    });
+  });
+
+  describe('when the method "last" is called', () => {
+    it('should return undefined if something different of a list is passed', () => {
+      expect(OhMyLib.last(1)).to.be.undefined;
+    });
+    it('should return undefined if an empty list is passed', () => {
+      expect(OhMyLib.last([])).to.be.undefined;
+    });
+    it('should return the unique element if a list with a single element is passed', () => {
+      expect(OhMyLib.last([5])).to.be.equal(5);
+    });
+    it('should return the last element if a list with more than one element is passed', () => {
+      expect(OhMyLib.last([5,10,2,9])).to.be.equal(9);
+    });
+  });
+
+  describe('when the method "init" is called', () => {
+    it('should return undefined if something different of a list is passed', () => {
+      expect(OhMyLib.init(1)).to.be.undefined;
+    });
+    it('should return an empty list if an empty list is passed', () => {
+      expect(OhMyLib.init([])).to.be.empty;
+    });
+    it('should return an empty list if a list with a single element is passed', () => {
+      expect(OhMyLib.init([5])).to.be.empty;
+    });
+    it('should return the head if a list with more than one element is passed', () => {
+      const list = [5,10,2,9];
+      expect(OhMyLib.init(list)).to.contains(5);
+      expect(OhMyLib.init(list)).to.contains(10);
+      expect(OhMyLib.init(list)).to.contains(2);
+    });
+  });
+
   describe('when the method "each" is called', () => {
     it('should run the iteratee for each element of the list', () => {
       let result = [];
